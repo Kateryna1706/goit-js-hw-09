cc1ee0ed2cc5d3fbca7252a043d2988db1802903
Kateryna1706 <silckatya@gmail.com>
2023-04-28 13:26:32 +0200

    did 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.body,
+};
 
+let timerId;
+
+refs.start.addEventListener('click', onStart);
+refs.stop.addEventListener('click', onStop);
+
+function onStart() {
+  timerId = setInterval(
+    () => (refs.body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+  refs.start.setAttribute('disabled', '');
+}
+
+function onStop() {
+  clearInterval(timerId);
+  refs.start.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
